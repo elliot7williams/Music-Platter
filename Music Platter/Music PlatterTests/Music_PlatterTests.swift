e0c730965664f059ba7786f373208c344328da0e
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 21:50:11 -0400

    Add Music Platter/Music PlatterTests/Music_PlatterTests.swift

---
+++ Music Platter/Music PlatterTests/Music_PlatterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlatterTests.swift
+//  Music PlatterTests
+//
+//  Created by Elliot Williams on 2025-08-24.
+//
+
+import Testing
+@testable import Music_Platter
+
+struct Music_PlatterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
