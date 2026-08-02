aaf680ad4f8d5bcf662561d08a45e649c7d15cdb
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 21:50:13 -0400

    Add Music Platter/Music Platter/Music_PlatterApp.swift

---
+++ Music Platter/Music Platter/Music_PlatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlatterApp.swift
+//  Music Platter
+//
+//  Created by Elliot Williams on 2025-08-24.
+//
+
+import SwiftUI
+
+@main
+struct Music_PlatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AlbumDiskView()
+        }
+    }
+}
